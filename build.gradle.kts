af7a9ec6ff8c6ca4290fe1474aa0ab3842e8ef6b
Krzysztof Kwapisiewicz <krzysztof.kwapisiewicz@fluxninja.com>
2022-09-29 14:39:26 +0200

    Prepare gradle for sonatype release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,34 @@
 import com.google.protobuf.gradle.*
+import java.time.Duration
 
 plugins {
     id("java")
     id("com.google.protobuf")
+    id("io.github.gradle-nexus.publish-plugin")
 }
 
 apply(from = "version.gradle.kts")
+
+nexusPublishing {
+    packageGroup.set("com.fluxninja.aperture")
+
+    repositories {
+        sonatype {
+            username.set(System.getenv("SONATYPE_USER"))
+            password.set(System.getenv("SONATYPE_KEY"))
+        }
+    }
+
+    connectTimeout.set(Duration.ofMinutes(5))
+    clientTimeout.set(Duration.ofMinutes(5))
+
+    transitionCheckOptions {
+        // We have many artifacts so Maven Central takes a long time on its compliance checks. This sets
+        // the timeout for waiting for the repository to close to a comfortable 50 minutes.
+        maxRetries.set(300)
+        delayBetween.set(Duration.ofSeconds(10))
+    }
+}
 
 subprojects {
     group = "com.fluxninja.aperture"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 pluginManagement {
     plugins {
         id("com.google.protobuf") version "0.8.17"
+        id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     }
 }
 dependencyResolutionManagement {
