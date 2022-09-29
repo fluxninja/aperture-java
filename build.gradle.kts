a555b8e6703774ff46fd796037d037f911266852
Krzysztof Kwapisiewicz <krzysztof.kwapisiewicz@fluxninja.com>
2022-09-29 18:43:48 +0200

    Set sonatype url

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ nexusPublishing {
 
     repositories {
         sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
             username.set(System.getenv("SONATYPE_USER"))
             password.set(System.getenv("SONATYPE_KEY"))
         }
