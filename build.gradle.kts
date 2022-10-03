ff526c768e34903503c7ea2aa455b990d5dafc91
Krzysztof Kwapisiewicz <krzysztof.kwapisiewicz@fluxninja.com>
2022-10-03 09:29:23 +0200

    Prepare for releasing to Maven Central (#12)
    * Use kotlin gradle
    
    * Prepare gradle for sonatype release
    
    * Prepare for publishing
    
    * Build on gradle files update
    
    * Set sonatype url
    
    * Publish snapshots from main
    
    * Fix cache key
    
    * Change version for releases
    
    * Use CircleCI context
    
    * Add gradle wrapper
    
    * Reformat GPG key
    
    We need to change `\n` to actual newline for the key to be read
    properly.

---
+++ build.gradle.kts
@@ -0,0 +1,117 @@
+import java.time.Duration
+
+plugins {
+    id("java")
+    id("com.google.protobuf")
+    id("io.github.gradle-nexus.publish-plugin")
+
+    `maven-publish`
+    signing
+}
+
+apply(from = "version.gradle.kts")
+
+nexusPublishing {
+    packageGroup.set("com.fluxninja.aperture")
+
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("SONATYPE_USER"))
+            password.set(System.getenv("SONATYPE_KEY"))
+        }
+    }
+
+    connectTimeout.set(Duration.ofMinutes(5))
+    clientTimeout.set(Duration.ofMinutes(5))
+
+    transitionCheckOptions {
+        // We have many artifacts so Maven Central takes a long time on its compliance checks. This sets
+        // the timeout for waiting for the repository to close to a comfortable 50 minutes.
+        maxRetries.set(300)
+        delayBetween.set(Duration.ofSeconds(10))
+    }
+}
+
+subprojects {
+    group = "com.fluxninja.aperture"
+}
+
+dependencies {
+    implementation(platform("io.opentelemetry:opentelemetry-bom-alpha:1.18.0-alpha"))
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp-logs")
+    implementation("io.opentelemetry:opentelemetry-sdk-logs")
+
+    // Workaround for @javax.annotation.Generated
+    // see: https://github.com/grpc/grpc-java/issues/3633
+    compileOnly("javax.annotation:javax.annotation-api:1.3.2")
+    compileOnly("io.grpc:grpc-api:1.44.0")
+    compileOnly("io.grpc:grpc-protobuf:1.44.0")
+    compileOnly("io.grpc:grpc-stub:1.44.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+// Publishing
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("mavenPublication") {
+            groupId = "com.fluxninja.aperture"
+            artifactId = "aperture-java"
+
+            plugins.withId("java-platform") {
+                from(components["javaPlatform"])
+            }
+            plugins.withId("java-library") {
+                from(components["java"])
+            }
+
+            versionMapping {
+                allVariants {
+                    fromResolutionResult()
+                }
+            }
+
+            pom {
+                name.set("Aperture Java")
+                description.set("Java SDK to connect to FluxNinja Aperture")
+                url.set("https://github.com/fluxninja/aperture-java")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("fluxninja")
+                        name.set("FluxNinja")
+                        url.set("https://github.com/fluxninja")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git@github.com:fluxninja/aperture-java.git")
+                    developerConnection.set("scm:git:git@github.com:fluxninja/aperture-java.git")
+                    url.set("git@github.com:fluxninja/aperture-java.git")
+                }
+            }
+        }
+    }
+}
+
+if (System.getenv("CI") != null) {
+    signing {
+        useInMemoryPgpKeys(System.getenv("GPG_PRIVATE_KEY"), System.getenv("GPG_PASSWORD"))
+        sign(publishing.publications["mavenPublication"])
+    }
+}
